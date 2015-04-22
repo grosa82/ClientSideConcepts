5d4856a655602de14aa2a1fc039d9e130a2c213a
grosa82 <grosa82@gmail.com>
2015-04-22 15:46:14 -0700

    Initial Commit

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+﻿function toggle() {
+    $("#result").toggle();
+}
+$(document).ready(function () {
+    $("#update").click(function () {
+        if (confirm("Are you sure you want to update the div?")) {
+            $("#result").css("backgroundColor", $("#color").val());
+            $("#result").html($("#text").val());
+        }
+    });
+});
